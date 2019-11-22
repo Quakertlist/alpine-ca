6eb4d52333fa3c33c8e08a0b46546be34813a552
sanghougui <sanghougui@vip.qq.com>
2019-11-22 12:01:31 +0800

    增加windows下编译脚本

--- build.bat
+++ build.bat
@@ -1,4 +1,8 @@
 SET VERSION=v1.0.1
+REM delete local image
 docker rmi quakertlist/alpine-ca:%VERSION%
+REM build new image
 docker build -t quakertlist/alpine-ca:%VERSION% .
+REM push image to remote registry
+docker push quakertlist/alpine-ca:%VERSION%
 @pause
